e8ad91ef93ada82d5f6c55dc1ff456fdfbab457a
Arnav Gupta <championswimmer@gmail.com>
2022-10-21 09:40:06 -0700

    go from one activity to another with intent
    Signed-off-by: Arnav Gupta <championswimmer@gmail.com>

--- app/src/main/java/com/scaler/droidmc22/MainActivity.kt
+++ app/src/main/java/com/scaler/droidmc22/MainActivity.kt
@@ -1,21 +1,27 @@
 package com.scaler.droidmc22
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    val TAG = "LifeCycle"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d(TAG, "onCreate: ")
         setContentView(R.layout.activity_main)
 
         val btnAdd = findViewById<Button>(R.id.btnAdd)
         val etNum1 = findViewById<EditText>(R.id.etNum1)
         val etNum2 = findViewById<EditText>(R.id.etNum2)
         val tvResult = findViewById<TextView>(R.id.tvResult)
+        val btnGoToWait = findViewById<Button>(R.id.btnGoToWait)
 
         btnAdd.setOnClickListener {
             try {
@@ -30,11 +36,38 @@ class MainActivity : AppCompatActivity() {
                     .setPositiveButton("OK", null)
                     .show()
             }
-
-
-
         }
 
+        btnGoToWait.setOnClickListener {
+            val intent = Intent(this, WaitActivity::class.java)
+            startActivity(intent)
+        }
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart: ")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume: ")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause: ")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop: ")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy: ")
     }
 
 }
---
+++ app/src/main/java/com/scaler/droidmc22/WaitActivity.kt
@@ -0,0 +1,11 @@
+package com.scaler.droidmc22
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class WaitActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_wait)
+    }
+}
