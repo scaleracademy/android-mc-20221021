8045553775a12b7914bdf80b62257f3804dc7ae0
Arnav Gupta <championswimmer@gmail.com>
2022-10-21 09:54:51 -0700

    handler to wait on ui thread
    Signed-off-by: Arnav Gupta <championswimmer@gmail.com>

--- app/src/main/java/com/scaler/droidmc22/WaitActivity.kt
+++ app/src/main/java/com/scaler/droidmc22/WaitActivity.kt
@@ -2,10 +2,35 @@ package com.scaler.droidmc22
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.widget.Button
+import android.widget.TextView
 
 class WaitActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_wait)
+
+        var counter = 0
+
+        val btnWait = findViewById<Button>(R.id.btnWait)
+        val tvMessage = findViewById<TextView>(R.id.tvMessage)
+
+        val btnCount = findViewById<Button>(R.id.btnCount)
+        val tvCount = findViewById<TextView>(R.id.tvCount)
+
+        btnWait.setOnClickListener {
+            tvMessage.text = "Wait for it..."
+
+            Handler().postDelayed({
+                tvMessage.text = "Done!"
+            }, 5000)
+
+        }
+
+        btnCount.setOnClickListener {
+            counter++
+            tvCount.text = counter.toString()
+        }
     }
 }
